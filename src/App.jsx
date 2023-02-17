8c1367ac0a259652ff3466ec48dfec9357190c66
Filip Trifunovic <114927397+filiptrifunovic-mile@users.noreply.github.com>
2023-02-17 15:52:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "https://a133.vercel.app";
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 
 axios.defaults.withCredentials = false;
 
